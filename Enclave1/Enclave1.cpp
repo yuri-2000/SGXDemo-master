1415fb4f665effdd4546fcf610582225dc4b38c7
xiaoyu niu <1051288025@qq.com>
2023-02-18 14:53:47 +0800

    添加项目文件。

---
+++ Enclave1/Enclave1.cpp
@@ -0,0 +1,13 @@
+#include "Enclave1_t.h"
+
+#include "sgx_trts.h"
+#include<string.h>
+
+void foo(char *buf, size_t len)
+{
+	const char *secret = "hello enclave!";
+	if (len > strlen(secret))
+	{
+		memcpy(buf,secret,strlen(secret)+1)
+	}
+}
---
+++ appdemo.cpp/appdemo.cpp
@@ -0,0 +1,27 @@
+﻿#include <stdio.h>
+#include <tchar.h>
+#include "sgx_urts.h"
+#include "Enclave1_u.h"
+#define ENCLAVE_FILE _T("../Debug/Enclave1.signed.dll")
+//#define ENCLAVE_FILE _T("../Debug/Enclave.signed.dll")
+#define MAX_BUF_LEN 100
+int main() {
+	sgx_enclave_id_t eid;
+	sgx_status_t ret = SGX_SUCCESS;
+	sgx_launch_token_t token = { 0 };
+	int updated = 0;
+	char buffer[MAX_BUF_LEN] = "Hello World!";
+	// Create the Enclave with above launch token.
+	ret = sgx_create_enclave(ENCLAVE_FILE, SGX_DEBUG_FLAG, &token, &updated, &eid, NULL);
+	if (ret != SGX_SUCCESS) {
+		printf("App: error %#x, failed to create enclave.\n", ret);
+		return -1;
+	}
+	// An Enclave call (ECALL) will happen here.
+	foo(eid, buffer, MAX_BUF_LEN);
+	printf("%s", buffer);
+	// Destroy the enclave when all Enclave calls finished.
+	if (SGX_SUCCESS != sgx_destroy_enclave(eid))
+		return -1;
+	return 0;
+}
